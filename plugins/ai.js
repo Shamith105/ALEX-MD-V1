4c12c18ea304c26031284d791db5177d0288f4f0
Arajuna <155340873+Arajuna@users.noreply.github.com>
2024-08-22 12:04:16 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,16 +1,18 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const { fetchJson} =requier('..lib/functions')
 
 cmd({
-    pattern: "ai.cht",
-    desc: "Check bot online or no.",
+    pattern: "ai",
+    desc: "ai chat",
     category: "main",
-    react: "👋🏻",
+    react: "🧠",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+let data= await fetchJson('')
 }catch(e){
 console.log(e)
 reply(`${e}`)
