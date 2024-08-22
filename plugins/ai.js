f6e709e45117a24d0d420b95b973145d298ac078
Arajuna <155340873+Arajuna@users.noreply.github.com>
2024-08-22 12:12:13 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -12,7 +12,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-let data= await fetchJson('')
+let data= await fetchJson('https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}')
 }catch(e){
 console.log(e)
 reply(`${e}`)
