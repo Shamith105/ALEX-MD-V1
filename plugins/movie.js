06c635912af0d8091790e76e59e299c2daa1f0c4
Arajuna <155340873+Arajuna@users.noreply.github.com>
2024-08-22 21:56:31 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -49,7 +49,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         // Send the movie information along with the poster image
         await conn.sendMessage(from, {
             image: { url: imageUrl },
-            caption: `${movieInfo}\n> BHASHI-MD`
+            caption: `${movieInfo}\n> ALEX-MD`
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
