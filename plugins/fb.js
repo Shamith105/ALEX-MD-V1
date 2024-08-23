f6c5a42b18f7309952f86f905b1c32614934891c
Arajuna <155340873+Arajuna@users.noreply.github.com>
2024-08-23 07:04:00 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,17 +1,17 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
-const yts = require('yt-search')
+const yts = require('facebook-search')
 
 
 cmd({
-    pattern: "video",
+    pattern: "fb",
     desc: "download videos.",
     category: "download",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please give me url or title")
+if(!q) return reply("> *Please give me url or title*")
 const search = await yts(q)
 const data = search.videos[0];
 const url = data.url
